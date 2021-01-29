d54764be6fc4744ad363a529d6da9604ae88afdc
Walter Geisler <soren.endon@gmail.com>
2021-01-28 20:13:33 -0600

    Attempting to use aws lambda asp net core extension

--- LambdaEntryPoint.cs
+++ LambdaEntryPoint.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Amazon.Lambda.AspNetCoreServer;
 
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 namespace AwsAspCore
--- Pages/Cognito.cshtml.cs
+++ Pages/Cognito.cshtml.cs
@@ -2,6 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
+using Amazon.AspNetCore.Identity.Cognito;
+using Amazon.Extensions.CognitoAuthentication;
+
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +14,46 @@ namespace AwsAspCore.Pages
 {
     public class CognitoModel : PageModel
     {
-        public void OnGet()
+        private SignInManager<CognitoUser> signInManager;
+        private CognitoUserManager<CognitoUser> userManager;
+
+        public CognitoModel(SignInManager<CognitoUser> signInManager, UserManager<CognitoUser> userManager)
         {
+            this.signInManager = signInManager;
+            this.userManager = userManager as CognitoUserManager<CognitoUser>;
+        }
+
+        public string Message { get; set; }
+
+        public bool SignedIn { get; set; }
+
+        public CognitoUser CUser { get; set; }
+
+        public async Task OnGetAsync(string message = null)
+        {
+            Message = message;
+            SignedIn = signInManager.IsSignedIn(User);
+            CUser = await userManager.GetUserAsync(User);
+        }
+
+        public async Task<ActionResult> OnPostLoginAsync(string email, string password, bool rememberMe = false)
+        {
+            var user = await userManager.FindByEmailAsync(email);
+            var result = await signInManager.PasswordSignInAsync(user, password, rememberMe, false);
+            if (result.Succeeded)
+            {
+                return RedirectToPage("/Cognito", new { message = "Login Success!" });
+            }
+            else
+            {
+                return RedirectToPage("/Cognito", new { message = "Login Failed!" });
+            }
+        }
+
+        public async Task<ActionResult> OnPostLogoutAsync()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToPage("/Cognito", new { message = "Logged Out!" });
         }
     }
 }
